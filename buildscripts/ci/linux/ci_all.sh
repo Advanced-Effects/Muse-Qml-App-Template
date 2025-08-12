3248b90e00f477f426fe3aba7f5a959aeed02557
kaixoo12 <208606493+kaixoo12@users.noreply.github.com>
2025-08-12 20:08:44 +0200

    rm: useless ci code

--- buildscripts/ci/linux/ci_all.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-HERE="$(cd "$(dirname "$0")" && pwd)"
-
-source $HOME/build_tools/environment.sh
-
-cmake -P $HERE/ci_all.cmake
--- buildscripts/ci/windows/ci_all.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-HERE="$(cd "$(dirname "$0")" && pwd)"
-
-source /c/build_tools/environment.sh
-
-cmake -P $HERE/ci_all.cmake
